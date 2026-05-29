d16aa8502708e1887047fb3126bd9160d103f675
Lopescamos <lopezmontalvokevin2021@gmail.com>
2026-05-29 12:29:19 -0500

    feat: add sistem general.

--- src/general/Sistema.java
+++ src/general/Sistema.java
@@ -1,13 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package general;
 
-/**
- *
- * @author lopez
- */
+import modelo.ClienteArreglo;
+import modelo.GradoEstudioArreglo;
+import modelo.OfertaArreglo;
+import modelo.PostulanteArreglo;
+import modelo.RubroArreglo;
+
 public class Sistema {
-    
+    public static ClienteArreglo clientes = new ClienteArreglo(50);
+    public static PostulanteArreglo postulantes = new PostulanteArreglo(100);
+    public static RubroArreglo rubros = new RubroArreglo(20);
+    public static GradoEstudioArreglo grados = new GradoEstudioArreglo(10);
+    public static OfertaArreglo ofertas = new OfertaArreglo(100);
+    public static Object conectado = null;
+    public static String tipoConectado = "";
 }
