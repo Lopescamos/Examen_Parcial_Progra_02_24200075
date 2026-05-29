458d2b398c1b6da7039894f0444840145bbb52f4
Lopescamos <lopezmontalvokevin2021@gmail.com>
2026-05-29 12:29:42 -0500

    feat: add app.

--- src/App.java
+++ src/App.java
@@ -1,11 +1,24 @@
+import controlador.ControladorLogin;
+import general.Sistema;
+import modelo.GradoEstudio;
+import modelo.Rubro;
+import view.FrmLogin;
 
 public class App {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Sistema.rubros.add(new Rubro("Tecnologia"));
+        Sistema.rubros.add(new Rubro("Finanzas"));
+        Sistema.rubros.add(new Rubro("Salud"));
+        Sistema.rubros.add(new Rubro("Educacion"));
+        Sistema.rubros.add(new Rubro("Retail"));
+
+        Sistema.grados.add(new GradoEstudio("Bachiller"));
+        Sistema.grados.add(new GradoEstudio("Magister"));
+        Sistema.grados.add(new GradoEstudio("Doctor"));
+        Sistema.grados.add(new GradoEstudio("Tecnico"));
+
+        FrmLogin fLogin = new FrmLogin();
+        ControladorLogin controlador = new ControladorLogin(fLogin);
+        controlador.iniciar();
     }
-    
 }
